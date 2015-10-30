5fc69efccce4ff34983d71b0f1bee1e95a21734b
Eugene Yeo <yeo.eugene.oey@gmail.com>
2015-10-30 23:08:57 +0800

    fix tab character

--- RemoteController/Source/RemoteController/RemoteController.Build.cs
+++ RemoteController/Source/RemoteController/RemoteController.Build.cs
@@ -3,11 +3,11 @@ using System.IO;
 
 public class RemoteController : ModuleRules
 {
-    public RemoteController(TargetInfo Target)
-    {
-        PrivateIncludePaths.AddRange(new string[] { "RemoteController/Private" });
-        PublicIncludePaths.AddRange(new string[] { "RemoteController/Public" });
+	public RemoteController(TargetInfo Target)
+	{
+		PrivateIncludePaths.AddRange(new string[] { "RemoteController/Private" });
+		PublicIncludePaths.AddRange(new string[] { "RemoteController/Public" });
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core" });
-    }
+		PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core" });
+	}
 }
