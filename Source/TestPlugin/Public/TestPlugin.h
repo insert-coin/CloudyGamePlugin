ae87c20c29eb5c6f8b2732a343b86986eb098310
ping <ngsp1994@gmail.com>
2015-11-10 02:22:56 +0800

    Cleaned up code

--- Source/TestPlugin/Public/TestPlugin.h
+++ Source/TestPlugin/Public/TestPlugin.h
@@ -20,29 +20,9 @@ public:
 	FSocket* ConnectionSocket;
 	FIPv4Endpoint RemoteAddressForConnection;
 
-	void Launch();
+	FSocket* FTestPluginModule::CreateTCPConnectionListener(const FString& SocketName, const FString& IP, const int32 Port, const int32 ReceiveBufferSize);
+	bool FTestPluginModule::FormatIP4ToNumber(const FString& TheIP, uint8(&Out)[4]);
+	FString FTestPluginModule::StringFromBinaryArray(const TArray<uint8>& BinaryArray);
+	FString FTestPluginModule::ReceiveMsg(FSocket* ListenerSocket);
 
-	bool StartTCPReceiver(
-		const FString& YourChosenSocketName,
-		const FString& TheIP,
-		const int32 ThePort
-		);
-
-	FSocket* CreateTCPConnectionListener(
-		const FString& YourChosenSocketName,
-		const FString& TheIP,
-		const int32 ThePort,
-		const int32 ReceiveBufferSize = 2 * 1024 * 1024
-		);
-
-	//Timer functions, could be threads
-	void TCPConnectionListener(); 	//can thread this eventually
-	void TCPSocketListener();		//can thread this eventually
-
-
-	//Format String IP4 to number array
-	bool FormatIP4ToNumber(const FString& TheIP, uint8(&Out)[4]);
-
-	//Rama's StringFromBinaryArray
-	FString StringFromBinaryArray(const TArray<uint8>& BinaryArray);
 };
