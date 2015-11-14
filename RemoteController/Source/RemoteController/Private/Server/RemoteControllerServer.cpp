78814f5ffbd1fc71fc03b812c08d51641223e67d
Eugene Yeo <yeo.eugene.oey@gmail.com>
2015-11-14 22:43:11 +0800

    Remove logging and unnecessary attribute

--- RemoteController/Source/RemoteController/Private/Server/RemoteControllerServer.cpp
+++ RemoteController/Source/RemoteController/Private/Server/RemoteControllerServer.cpp
@@ -26,39 +26,20 @@ bool RemoteControllerServer::StartServer(const FIPv4Endpoint& endpoint)
 
 void RemoteControllerServer::HandleInputReceived(const FArrayReaderPtr& Data, const FIPv4Endpoint& Sender)
 {
-
 	FUdpRemoteControllerSegment::FKeyBoardChunk Chunk;
 	*Data << Chunk;
-
-	UE_LOG(ServerLog, Warning, TEXT("CloudyGame: RemoteController Handling Data"));
 	char* charData = (char*)Data->GetData(); 
-	UE_LOG(ServerLog, Warning, TEXT("Data: %s"), UTF8_TO_TCHAR(charData));
 	UGameInstance* gameinstance = GEngine->GameViewport->GetGameInstance();
-	UE_LOG(ServerLog, Warning, TEXT("Game Instance found"));
 	UWorld* world = gameinstance->GetWorld();
-	//APlayerController* controller = UGameplayStatics::GetPlayerController(world, Chunk.ControllerID);
 	APlayerController* controller = gameinstance->GetFirstLocalPlayerController();
-	UE_LOG(ServerLog, Warning, TEXT("Player controller [%d] found"), Chunk.ControllerID);
-	
-	UE_LOG(ServerLog, Warning, TEXT("KEY %d"), Chunk.KeyCode);
 	EInputEvent ie;
 	if (Chunk.InputEvent == 2){ // Pressed
 		ie = EInputEvent::IE_Pressed;
-		UE_LOG(ServerLog, Warning, TEXT("Input Pressed"));
 	}
 	else if (Chunk.InputEvent == 3){ // Released
 		ie = EInputEvent::IE_Released;
-		UE_LOG(ServerLog, Warning, TEXT("Input Released"));
 	}
-
-	
-	//UE_LOG(ServerLog, Warning, TEXT("Sequence %d"), Chunk.Sequence);
 	FInputKeyManager manager = FInputKeyManager::Get();
 	FKey key = manager.GetKeyFromCodes(Chunk.KeyCode, Chunk.KeyCode);
-	//UE_LOG(ServerLog, Warning, TEXT("Input Key Executed %d"), Chunk.KeyCode);
-	//UE_LOG(ServerLog, Warning, TEXT("Unicode %d"), Chunk.Unicode);
-	//FKey key = FKey::FKey(UTF8_TO_TCHAR(Chunk.Unicode));
 	controller->InputKey(key, ie, 1, false);
-	//controller->InputKey(FKey(UTF8_TO_TCHAR("w")), ie, 1, false);
-	UE_LOG(ServerLog, Warning, TEXT("Done"));
 }
