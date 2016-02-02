da507099faabac4b0bf68abe3cb41ca8e525ec55
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-02-03 00:16:02 +0800

    extract logic of devices into separate methods

--- RemoteController/Source/RemoteController/Private/Shared/UdpRemoteControllerSegment.h
+++ RemoteController/Source/RemoteController/Private/Shared/UdpRemoteControllerSegment.h
@@ -1,24 +1,66 @@
+namespace EUdpRemoteControllerSegment
+{
+	enum Type
+	{
+		None = 0,
+		KeyboardInput = 1,
+		MouseInput = 2,
+		Data = 3
+	};
+}
+
 namespace FUdpRemoteControllerSegment {
-	struct FKeyBoardChunk 
+	struct FHeaderChunk
 	{
 		uint8 Version;
-		uint8 InputType;
+
+		/**
+		 * Segment Type
+		 * @see EUdpRemoteControllerSegment
+		 */
+		uint8 SegmentType;
+
+		friend FArchive& operator<<(FArchive& Ar, FHeaderChunk& Chunk)
+		{
+			return Ar
+				<< Chunk.Version
+				<< Chunk.SegmentType;
+		}
+	};
+
+	struct FKeyboardInputChunk
+	{
 		uint32 Sequence;
 		uint8 ControllerID;
 		int32 KeyCode;
 		int32 CharCode;
 		uint8 InputEvent;
 
-		friend FArchive& operator<<(FArchive& Ar, FKeyBoardChunk& Chunk)
+		friend FArchive& operator<<(FArchive& Ar, FKeyboardInputChunk& Chunk)
 		{
 			return Ar
-				<< Chunk.Version
-				<< Chunk.InputType
 				<< Chunk.Sequence
 				<< Chunk.ControllerID
 				<< Chunk.KeyCode
 				<< Chunk.CharCode
 				<< Chunk.InputEvent;
+		}
+	};
+
+	struct FMouseInputChunk
+	{
+		uint32 Sequence;
+		uint8 ControllerID;
+		int32 XAxis;
+		int32 YAxis;
+
+		friend FArchive& operator<<(FArchive& Ar, FMouseInputChunk& Chunk)
+		{
+			return Ar
+				<< Chunk.Sequence
+				<< Chunk.ControllerID
+				<< Chunk.XAxis
+				<< Chunk.YAxis;
 		}
 	};
 }
