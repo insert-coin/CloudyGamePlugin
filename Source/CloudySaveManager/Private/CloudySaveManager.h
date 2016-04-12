fd051501e230ea77c3707c920843fbd920d8a8d1
Joel Ng <blood_elves88@yahoo.com.sg>
2016-04-12 18:52:34 +0800

    Delete comments from private header file for CloudySaveManager. Updated comments on the public header file

--- Source/CloudySaveManager/Private/CloudySaveManager.h
+++ Source/CloudySaveManager/Private/CloudySaveManager.h
@@ -12,26 +12,8 @@ public:
     void StartupModule();
     void ShutdownModule();
  
-    /**
-    *   Save the contents of the SaveGameObject to a slot, and upload it to CloudyGame's cloud.
-    *   @param SaveGameObject   Object that contains data about the save game that we want to write out
-    *   @param SlotName         Name of save game slot to save to.
-    *   @param UserIndex        For some platforms, master user index to identify the user doing the saving.
-    *   @param PCID             PlayerController object of the player to save
-    *   
-    *   @return                 Whether we successfully saved this information
-    */
-    UFUNCTION(BlueprintCallable, Category = "CloudyGame")
-        bool Cloudy_SaveGameToSlot(USaveGame* SaveGameObject, const FString& SlotName, 
+    bool Cloudy_SaveGameToSlot(USaveGame* SaveGameObject, const FString& SlotName, 
                                    const int32 UserIndex, const int32 PCID);
 
-    /**
-    *   Download the save file from CloudyGame's cloud, then save the contents of the SaveGameObject to a slot.
-    *   @param SlotName             Name of save game slot to save to.
-    *   @param UserIndex            For some platforms, master user index to identify the user doing the saving.
-    *   
-    *   @return SaveGameObject      Object containing loaded game state (NULL if load fails)
-    */
-    UFUNCTION(BlueprintCallable, Category = "CloudyGame")
-        USaveGame* Cloudy_LoadGameFromSlot(const FString& SlotName, const int32 UserIndex, const int32 PCID);
+    USaveGame* Cloudy_LoadGameFromSlot(const FString& SlotName, const int32 UserIndex, const int32 PCID);
 };
